084cbfb55b17d4fda47836b335e08e11b6dea917
Josh <joshua.ng2332@gmail.com>
2020-07-08 17:23:15 +0100

    will allow the temp to only go to 25

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,17 +1,41 @@
 describe('Thermostat', function() {
   var thermostat;
+
   beforeEach(function() {
     thermostat = new Thermostat();
   });
+
   it("should start with temperature of 20", function() {
     expect(thermostat.temp).toEqual(20);
   });
+
   it("can increase temp by 1 when up funtion called with input 1", function() {
     thermostat.up(1);
     expect(thermostat.temp).toEqual(21);
   })
+
   it("can decrease temp by 1 when down funtion called with input 1", function() {
     thermostat.down(1);
     expect(thermostat.temp).toEqual(19);
   })
+
+  it("can't decrease less than 10", function() {
+    thermostat.down(11);
+    expect(thermostat.temp).toEqual(10);
+  });
+
+  describe("If power saving is off", function(){
+    it("will allow the temp to go to 32", function(){
+      thermostat.powerSaving = false
+      thermostat.up(13);
+      expect(thermostat.temp).toEqual(32);
+    });
+  });
+  describe("If power saving is on", function(){
+    it("will allow the temp to only go to 25", function(){
+      thermostat.powerSaving = true
+      thermostat.up(13);
+      expect(thermostat.temp).toEqual(25);
+    });
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,11 +1,25 @@
 class Thermostat{
   constructor(){
     this.temp = 20;
-  }
+    this.min = 10;
+    this.max = 32;
+    this.powerSaving = false
+  };
+
+  tempLimit(temp, min, max){ 
+    // console.log(temp < min ? min : (temp > max ? max : temp))
+    this.temp = temp < min ? min : (temp > max ? max : temp);
+  };
+
   up(input){
-    this.temp +=input;
-  }
+    (this.powerSaving === true) ? this.tempLimit(this.temp +=input,10,25) : this.tempLimit(this.temp +=input,10,32);
+    // if (this.powerSaving == true) {
+    //   this.temp = this.tempLimit(this.temp +=input,10,25)
+    // } else {
+    //   this.temp = this.tempLimit(this.temp +=input,10,32)
+    }
+  
   down(input){
-    this.temp -=input;
-  }
+    this.tempLimit(this.temp -=input,10,32)
+  };
 };
